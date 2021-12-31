30426552674e42f59c1f7017497665264dafa90a
binhlb <binhlb179@gmail.com>
2021-12-31 08:37:35 -0500

    Initial commit

---
+++ BasicHttpsClientControlByPWMAuthenticationSendPulseV4/BasicHttpsClientControlByPWMAuthenticationSendPulseV4.ino
@@ -0,0 +1,228 @@
+/**
+   BasicHTTPSClient.ino
+
+    Created on: 14.10.2018
+
+*/
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+
+#include <WiFiClientSecure.h>
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+
+// This is GandiStandardSSLCA2.pem, the root Certificate Authority that signed 
+// the server certifcate for the demo server https://jigsaw.w3.org in this
+// example. This certificate is valid until Sep 11 23:59:59 2024 GMT
+/*const char* rootCACertificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"-----END CERTIFICATE-----\n";*/
+
+// Not sure if WiFiClientSecure checks the validity date of the certificate. 
+// Setting clock just to be sure...
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void setClock() {
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print(F("Waiting for NTP time sync: "));
+  time_t nowSecs = time(nullptr);
+  while (nowSecs < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(F("."));
+    yield();
+    nowSecs = time(nullptr);
+  }
+
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&nowSecs, &timeinfo);
+  Serial.print(F("Current time: "));
+  Serial.print(asctime(&timeinfo));
+}
+
+
+WiFiMulti WiFiMulti;
+int ledState = LOW;
+const int ledPin0 =  21;
+//const int ledPin1 =  12;
+//const int ledPin2 =  27;
+//const int ledPin3 =  33;
+//const int ledPin4 =  32;
+
+void setup() {
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("Niagara Research_2.4GHz", "Thenrnetwork24!");
+
+  // wait for WiFi connection
+  Serial.print("Waiting for WiFi to connect...");
+  while ((WiFiMulti.run() != WL_CONNECTED)) {
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(1600);  // This is the maximum PWM frequency
+  Wire.setClock(400000);
+
+  setClock();  
+
+  // set the digital pin as output:
+  pinMode(ledPin0, OUTPUT);
+  //pinMode(ledPin1, OUTPUT);
+  //pinMode(ledPin2, OUTPUT);
+  //pinMode(ledPin3, OUTPUT);
+  //pinMode(ledPin4, OUTPUT);
+
+  
+
+  
+}
+bool flag0 = true;
+bool flag1 = true;
+bool flag2 = true;
+bool flag3 = true;
+bool flag4 = true;
+
+
+void loop() {
+  WiFiClientSecure *client = new WiFiClientSecure;
+  struct clientData{
+    char name[10];
+    
+    };
+  const size_t bufferSize = JSON_ARRAY_SIZE(5) + 5*JSON_OBJECT_SIZE(3) + 5*JSON_OBJECT_SIZE(6) + 1000;
+  DynamicJsonDocument doc(bufferSize);
+ 
+  if(client) {
+    //client -> setCACert(rootCACertificate);
+
+    {
+      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+      HTTPClient https;
+  
+      Serial.print("[HTTPS] begin...\n");
+      String server = "https://vendalitemvcwebapiv4.azurewebsites.net/api/candlesapiESP32/AllByChurch/1";
+      https.begin(*client, server);
+      https.addHeader("Authorization","Basic YWRtaW4xQG91dGxvb2suY29tOnBhc3N3b3Jk");
+      if (true) {  // HTTPS
+        Serial.print("[HTTPS] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = https.GET();
+  
+        // httpCode will be negative on error
+        if (httpCode > 0) {
+          // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+  
+          // file found at server
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            String payload = https.getString();
+            deserializeJson(doc, payload);
+            String ledcheck0 = doc[0]["active"];
+            String ledcheck1 = doc[1]["active"];
+            String ledcheck2 = doc[2]["active"];
+            String ledcheck3 = doc[3]["active"];
+            String ledcheck4 = doc[4]["active"];
+            String ledcheck5 = doc[5]["active"];
+            Serial.println(ledcheck0);
+            Serial.println(flag0);
+            if(ledcheck0 == "true" && flag0 == true){
+              ledState = HIGH;
+              digitalWrite(ledPin0, ledState);
+              delay(100);
+              ledState = LOW;
+              digitalWrite(ledPin0, ledState);
+              delay(1000);
+              flag0 = false;
+            }
+
+            if(ledcheck1 == "true" && flag1 == true){
+              ledState = HIGH;
+              digitalWrite(ledPin0, ledState);
+              delay(100);
+              ledState = LOW;
+              digitalWrite(ledPin0, ledState);
+              delay(1000);
+              flag1 = false;
+            }
+
+            if(ledcheck2 == "true" && flag2 == true){
+              ledState = HIGH;
+              digitalWrite(ledPin0, ledState);
+              delay(100);
+              ledState = LOW;
+              digitalWrite(ledPin0, ledState);
+              delay(1000);
+              flag2 = false;
+            }
+
+            if(ledcheck3 == "true" && flag3 == true){
+              ledState = HIGH;
+              digitalWrite(ledPin0, ledState);
+              delay(100);
+              ledState = LOW;
+              digitalWrite(ledPin0, ledState);
+              delay(1000);
+              flag3 = false;
+            }
+
+            if(ledcheck4 == "true" && flag4 == true){
+              ledState = HIGH;
+              digitalWrite(ledPin0, ledState);
+              delay(100);
+              ledState = LOW;
+              digitalWrite(ledPin0, ledState);
+              delay(1000);
+              flag4 = false;
+            }
+            //else
+            //{
+              //ledState = LOW;
+              //digitalWrite(ledPin0, ledState);
+            //}
+            //digitalWrite(ledPin0, ledState);
+
+            
+            //digitalWrite(ledPin4, ledState);
+          }
+        } else {
+          Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        }
+  
+        https.end();
+      } else {
+        Serial.printf("[HTTPS] Unable to connect\n");
+      }
+
+      // End extra scoping block
+    }
+  
+    delete client;
+  } else {
+    Serial.println("Unable to create client");
+  }
+
+  Serial.println();
+  Serial.println("Waiting 1s before the next round...");
+  Serial.println("This is new software version");
+  delay(1000);
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# IoT-ElectronicCandles
+ ESP32 device
